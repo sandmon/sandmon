82f4b04372b1d5a2112fd3e66d0baf371c90d7f3
Koyeo <im@koyeo.io>
2021-02-03 08:20:58 +0800

    support hight

--- projects/site/src/app/components/example/example.component.ts
+++ projects/site/src/app/components/example/example.component.ts
@@ -1,6 +1,32 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {SafeHtml} from '@angular/platform-browser';
+import {AfterViewInit, Component, Input, OnInit} from '@angular/core';
+import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
+import Prism from 'prismjs';
+import 'prismjs/components/prism-typescript.min';
+import 'prismjs/components/prism-scss.min';
 
+Prism.languages.angular = Prism.languages.extend('typescript', {});
+Prism.languages.insertBefore('angular', 'string', {
+  'template-string': {
+    pattern: /template[\s]*:[\s]*`(?:\\[\s\S]|[^\\`])*`/,
+    greedy: true,
+    inside: {
+      'html': {
+        pattern: /`(?:\\[\s\S]|[^\\`])*`/,
+        inside: Prism.languages.html
+      }
+    }
+  },
+  'styles-string': {
+    pattern: /styles[\s]*:[\s]*\[[\s]*`(?:\\[\s\S]|[^\\`])*`[\s]*\]/,
+    greedy: true,
+    inside: {
+      'scss': {
+        pattern: /`(?:\\[\s\S]|[^\\`])*`/,
+        inside: Prism.languages.scss
+      }
+    }
+  }
+});
 
 @Component({
   selector: 'example',
@@ -12,12 +38,18 @@ export class ExampleComponent implements OnInit {
   @Input() title: string;
   @Input() description: SafeHtml;
   @Input() code: string;
+  highlightCode: SafeHtml;
   showCode: boolean = false;
 
-  constructor() {
+  constructor(
+    private domSanitizer: DomSanitizer,
+  ) {
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.highlightCode = this.domSanitizer.bypassSecurityTrustHtml(
+      Prism.highlight(this.code, Prism.languages.angular)
+    );
   }
 
   toggleCode() {
